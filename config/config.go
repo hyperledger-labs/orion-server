1b6152d1ef02a128accf1a4baff0a71b11e5557a
Artem Barger <bartem@il.ibm.com>
2021-03-01 19:06:30 +0200

    Add license headers to all go files
    Signed-off-by: Artem Barger <bartem@il.ibm.com>

--- config/config.go
+++ config/config.go
@@ -1,3 +1,5 @@
+// Copyright IBM Corp. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 package config
 
 import (
--- pkg/types/http.go
+++ pkg/types/http.go
@@ -1,3 +1,5 @@
+// Copyright IBM Corp. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 package types
 
 // HttpResponseErr holds an error message. It is used as the body of an http error response.
