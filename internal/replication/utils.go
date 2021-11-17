68ef28e2e1f86e8c648c23853b4c37aa9d85e3c7
Yoav Tock <tock@il.ibm.com>
2021-11-17 14:27:50 +0200

    ConsensusConfig reconfig rules
    Define the rules for updating the ConsensusConfig, in particular, the Memebrs.
    
    The core contribution is the VerifyConsensusReConfig method, that checks the configuration changes in types.ConsensusConfig.
    
    This method checks that the changes between one ConsensusConfig to the next are safe, because some mutations might
    cause a permanent loss of quorum in the cluster, something that is very difficult to recover from.
     - Members can be added or removed (membership change) one member at a time
     - Members' endpoints cannot be changed together with a membership change
     - An existing member cannot change its Raft ID (it must be removed from the cluster and added again as a new member)
     - The Raft ID of a new member must be unique - therefore it must be larger than MaxRaftId
    
    We assume that both the current and updated ClusterConfig are internally consistent, specifically, that the Nodes
    and the ConsensusConfig.Members arrays match by NodeId in each.
    
    Signed-off-by: Yoav Tock <tock@il.ibm.com>

--- internal/replication/utils.go
+++ internal/replication/utils.go
@@ -7,8 +7,10 @@ import (
 	"fmt"
 	"hash/crc64"
 
-	"github.com/hyperledger-labs/orion-server/pkg/types"
 	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger-labs/orion-server/pkg/logger"
+	"github.com/hyperledger-labs/orion-server/pkg/types"
+	"github.com/pkg/errors"
 	"go.etcd.io/etcd/raft"
 	"go.etcd.io/etcd/raft/raftpb"
 )
@@ -28,7 +30,86 @@ func raftEntryString(e raftpb.Entry) string {
 		e.Term, e.Index, e.Type, len(e.Data), h.Sum64())
 }
 
+// VerifyConsensusReConfig checks the configuration changes in types.ConsensusConfig.
+//
+// This method checks that the changes between one ConsensusConfig to the next are safe, because some mutations might
+// cause a permanent loss of quorum in the cluster, something that is very difficult to recover from.
+// - Members can be added or removed (membership change) one member at a time
+// - Members' endpoints cannot be changed together with a membership change
+// - An existing member cannot change its Raft ID (it must be removed from the cluster and added again as a new member)
+// - The Raft ID of a new member must be unique - therefore it must be larger than MaxRaftId
+//
+// We assume that both the current and updated ClusterConfig are internally consistent, specifically, that the Nodes
+// and the ConsensusConfig.Members arrays match by NodeId in each.
+func VerifyConsensusReConfig(currentConfig, updatedConfig *types.ConsensusConfig, lg *logger.SugarLogger) error {
+	addedPeers, removedPeers, changedPeers, err := detectPeerConfigChanges(currentConfig, updatedConfig)
+	if err != nil {
+		return err
+	}
+
+	if len(addedPeers)+len(removedPeers) > 1 {
+		return errors.Errorf("cannot make more than one membership change at a time: %d added, %d removed", len(addedPeers), len(removedPeers))
+	}
+
+	if (len(addedPeers)+len(removedPeers) == 1) && len(changedPeers) > 1 {
+		return errors.Errorf("cannot update peer endpoints while making membership changes: %d added, %d removed, %d updated", len(addedPeers), len(removedPeers), len(changedPeers))
+	}
+
+	if len(addedPeers)+len(removedPeers) == 1 {
+		// TODO support dynamic membership changes, see:
+		return errors.New("dynamic membership changes to the cluster are not supported yet")
+	}
+
+	if !proto.Equal(currentConfig.RaftConfig, updatedConfig.RaftConfig) {
+		lg.Warning("ConsensusConfig RaftConfig changed, the new RaftConfig will be applied after server restart!")
+	}
+
+	return nil
+}
+
+func detectPeerConfigChanges(currentConfig, updatedConfig *types.ConsensusConfig) (addedPeers, removedPeers, changedPeers []*types.PeerConfig, err error) {
+	currPeers := make(map[string]*types.PeerConfig)
+	for _, m := range currentConfig.Members {
+		currPeers[m.NodeId] = m
+	}
+
+	updtPeers := make(map[string]*types.PeerConfig)
+	for _, updtMember := range updatedConfig.Members {
+		updtPeers[updtMember.NodeId] = updtMember
+		if currMember, ok := currPeers[updtMember.NodeId]; ok {
+			// existing peer
+			if !proto.Equal(updtMember, currMember) {
+				if updtMember.RaftId != currMember.RaftId {
+					return nil, nil, nil,
+						errors.Errorf("cannot change the RaftId of an existing peer: NodeId=%s, current=%d, updated=%d",
+							currMember.NodeId, currMember.RaftId, updtMember.RaftId)
+				}
+				changedPeers = append(changedPeers, updtMember) //endpoint changed
+			}
+		} else {
+			// added peer
+			if updtMember.RaftId <= currentConfig.RaftConfig.MaxRaftId {
+				return nil, nil, nil,
+					errors.Errorf("the RaftId of a new peer must be unique,  > MaxRaftId [%d]; but: NodeId=%s, RaftID=%d",
+						currentConfig.RaftConfig.MaxRaftId, updtMember.NodeId, updtMember.RaftId)
+			}
+			addedPeers = append(addedPeers, updtMember)
+		}
+	}
+
+	for _, currMember := range currPeers {
+		if _, ok := updtPeers[currMember.NodeId]; !ok {
+			// removed peer
+			removedPeers = append(removedPeers, currMember)
+		}
+	}
+
+	return
+}
+
 // ClassifyClusterReConfig detects the kind of changes that happened in the ClusterConfig.
+// We assume that both the current and updated config are internally consistent (valid), but not necessarily with
+// respect to each other.
 func ClassifyClusterReConfig(currentConfig, updatedConfig *types.ClusterConfig) (nodes bool, consensus bool, ca bool, admins bool) {
 	nodes = changedNodes(currentConfig.GetNodes(), updatedConfig.GetNodes())
 
