06e7c853838fb6dd57386bba138186e2a591ebbe
Yoav Tock <tock@il.ibm.com>
2022-04-27 14:48:59 +0300

    Fixing tls certificates generation script and local docker multiarch (#382)
    Signed-off-by: Gennady Laventman <gennady@il.ibm.com>
    Signed-off-by: Yoav Tock <tock@il.ibm.com>
    
    Co-authored-by: Gennady Laventman <gennady@il.ibm.com>

--- scripts/tlsGen.sh
+++ scripts/tlsGen.sh
@@ -3,13 +3,14 @@ create_pki() {
         mkdir -p "$BASE_DIR/tls/$1"
 
         echo "Generating private key for $1"
-        docker run -it --rm -v $BASE_DIR/tls:/export frapsoft/openssl ecparam -name prime256v1 -genkey -noout -out "/export/$1/$1.key"
+        docker run -it --rm -v $BASE_DIR/tls:/export nginx openssl ecparam -name prime256v1 -genkey -noout -out "/export/$1/$1.key"
+        docker run -it --rm -v $BASE_DIR/tls:/export nginx chmod ga+r "/export/$1/$1.key"
 
         echo "Generate node CSR"
-        docker run -it --rm -v $BASE_DIR/tls:/export frapsoft/openssl req -new -key "/export/$1/$1.key" -out "/export/$1/$1.csr" -subj "/C=IL/ST=Haifa/O=BCDB"
+        docker run -it --rm -v $BASE_DIR/tls:/export nginx openssl req -new -key "/export/$1/$1.key" -out "/export/$1/$1.csr" -subj "/C=IL/ST=Haifa/O=BCDB"
 
         echo "Generate node certificate"
-        docker run -it --rm -v $BASE_DIR/tls:/export frapsoft/openssl x509 -req -in "/export/$1/$1.csr" -CA "/export/CA/CA.pem" -CAkey "/export/CA/CA.key" -CAcreateserial -out "/export/$1/$1.pem" -days 365 -sha256
+        docker run -it --rm -v $BASE_DIR/tls:/export nginx openssl x509 -req -in "/export/$1/$1.csr" -CA "/export/CA/CA.pem" -CAkey "/export/CA/CA.key" -CAcreateserial -out "/export/$1/$1.pem" -days 365 -sha256
 }
 
 if [ -z "$1" ]
@@ -32,10 +33,11 @@ echo "Creating PKIs folders"
 mkdir -p "$BASE_DIR/tls/CA"
 
 echo "Generate root CA private key"
-docker run -it --rm -v $BASE_DIR/tls:/export frapsoft/openssl ecparam -name prime256v1 -genkey -noout -out "/export/CA/CA.key"
+docker run -it --rm -v $BASE_DIR/tls:/export nginx openssl ecparam -name prime256v1 -genkey -noout -out "/export/CA/CA.key"
+docker run -it --rm -v $BASE_DIR/tls:/export nginx chmod ga+r "/export/CA/CA.key"
 
 echo "Generating self-signed root CA certificate"
-docker run -it --rm -v $BASE_DIR/tls:/export frapsoft/openssl req -new -x509 -nodes -key "/export/CA/CA.key" -sha256 -days 365 -out "/export/CA/CA.pem" -subj "/C=IL/ST=Haifa/O=BCDB" -extensions v3_ca
+docker run -it --rm -v $BASE_DIR/tls:/export nginx openssl req -new -x509 -nodes -key "/export/CA/CA.key" -sha256 -days 365 -out "/export/CA/CA.pem" -subj "/C=IL/ST=Haifa/O=BCDB" -extensions v3_ca
 
 create_pki "server"
 
